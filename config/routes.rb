b627bc751ec34d167d2dd0e2a150f23656179286
grant brooks <grantbrooks75@gmail.com>
2015-12-30 23:32:57 +1100

    added stripe charges form, added charges controller and initilizer for stripe and added figaro gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
+  # for stripe payments
+  resources :charges
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
